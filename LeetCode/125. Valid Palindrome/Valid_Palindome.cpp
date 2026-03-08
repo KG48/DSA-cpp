f26b6cc6b60cac5baa15ff3ecc5422d0e873516e
KG48 <karangavhane1213@gmail.com>
2026-03-08 23:54:42 +0530

    Update

---
+++ LeetCode/125. Valid Palindrome/Valid_Palindome.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string s) {
+        string newStr = "";
+        for (char i : s) {
+            if (isalnum(i)) {
+                newStr += tolower(i);
+            }
+        }
+        int l = 0, r = newStr.size() - 1;
+        while (l < r) {
+            if (newStr[l++] != newStr[r--]) return false;
+        }
+        return true;
+    }
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s = "aba";
+
+    cout << isPalindrome(s);
+    return 0;
+}
